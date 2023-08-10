29d45215829ec1502c33931010ac28a179888087
Vishu Mishra <mishra.vishu99@gmail.com>
2023-08-10 10:14:54 +0530

    Initial commit

---
+++ api/api.py
@@ -0,0 +1,96 @@
+from fastapi import FastAPI, HTTPException
+import aio_pika
+import logging
+import asyncio,random
+
+app = FastAPI()
+
+async def publish_to_queue(queue_name: str, message: str):
+    connection = await aio_pika.connect_robust(host="rabbitmq",port=5672)
+    channel = await connection.channel()
+    await channel.declare_queue(queue_name)
+    await channel.default_exchange.publish(
+        aio_pika.Message(body=message.encode()),
+        routing_key=queue_name 
+    )
+    await connection.close()
+
+
+async def get_processed_message_from_queue(queue_name: str):
+    connection = await aio_pika.connect_robust(host="rabbitmq",port=5672)
+    channel = await connection.channel()
+    queue = await channel.declare_queue(queue_name)
+    try:
+        message = await queue.get()
+        logging.info("Message in output_queue: ",message)
+        
+        if message:
+            processed_message = message.body.decode()
+            await message.ack()  
+        else:
+            processed_message = None
+        return True, processed_message  
+    except Exception as e:
+        logging.error("output queue exception block: "+str(e),exc_info=True)
+        return False, None       
+    await connection.close()
+    
+@app.post("/message/")
+async def get_display_message(message:str):
+    try:
+        
+        await publish_to_queue("input_queue", message)
+        print({"message": f"{message} sent for processing"})
+        processed_message = await get_processed_message_from_queue("output_queue")
+        if processed_message[0]:
+            return {"processed_message": processed_message[1]}
+        else:
+            return {"message": "No processed messages available"}
+        
+    except Exception as e:
+        logging.error(str(e),exc_info=True)
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+
+
+# @app.post("/process_message/")
+# async def process_message(message: str):
+#     try:
+#         await publish_to_queue("input_queue", message)
+#         return {"message": "Message sent for processing"}
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
+
+# @app.get("/processed_message/")
+# async def get_processed_message():
+#     processed_message = await get_processed_message_from_queue("output_queue")
+#     if processed_message:
+#         return {"processed_message": processed_message}
+#     else:
+#         return {"message": "No processed messages available"}
+
+
+#just for practice tried to handle both get and post request using only 1 URL
+# @app.post("/messages/")
+# @app.get("/messages/")
+# async def get_and_display_messages(request:Request):
+#     if request.method == 'POST':
+#         try:
+#             request_body  = await request.json()
+#             message = request_body.get('message')
+#             await publish_to_queue("input_queue", message)
+#             return {"message": "Message sent for processing"}
+#         except Exception as e:
+#             raise HTTPException(status_code=500, detail=str(e))
+        
+#     elif request.method == 'GET':
+#         processed_message = await get_processed_message_from_queue("output_queue")
+#         if processed_message:
+#             return {"processed_message": processed_message}
+#         else:
+#             return {"message": "No processed messages available"}
+
+
+
+
---
+++ processor/processor.py
@@ -0,0 +1,42 @@
+import aio_pika
+import asyncio,random
+
+async def process_message(message: str):
+    # Simulate a delay in processing
+    print(f"Processing message : {message}")
+    processing_time = random.randint(3,10)
+    await asyncio.sleep(processing_time)
+    print(f"Processed message : {message}")
+    return f"{message} World"
+
+
+async def on_message(message: aio_pika.IncomingMessage):
+    async with message.process():
+        body = message.body.decode()
+        processed_message = await process_message(body)
+        connection = await aio_pika.connect_robust(host="172.26.0.4",port=5672)
+        channel = await connection.channel()
+        await channel.declare_queue("output_queue")
+        print(f"Sending processed message : {processed_message}")
+        # Use the default exchange to publish the processed message to the output_queue
+        await channel.default_exchange.publish(
+            aio_pika.Message(body=processed_message.encode()),
+            routing_key="output_queue"
+        )
+        print(f"Sent processed message : {processed_message}")
+        await connection.close()
+
+
+async def main():
+    connection = await aio_pika.connect_robust(host="172.26.0.4",port=5672)
+    channel = await connection.channel()
+    input_queue = await channel.declare_queue("input_queue")
+    await channel.set_qos(prefetch_count=1)
+    await input_queue.consume(on_message)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+    loop.run_forever()
+    # asyncio.run(main())
